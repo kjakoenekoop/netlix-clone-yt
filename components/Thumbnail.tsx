3297254b4273e2fb27e8efa8914d75f551872104
Koen <k.j.a.koenekoop@gmail.com>
2022-07-11 13:20:24 +0700

    Fixed typo thumbnail scaling

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Thumbnail = ({movie}: Props) => {
     return (
         <div
-            className={"relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-106"}>
+            className={"relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"}>
             <Image
                 src={`${BASE_URL}${movie.backdrop_path || movie.poster_path}`}
                 className={"rounded-sm object-cover md:rounded"}
